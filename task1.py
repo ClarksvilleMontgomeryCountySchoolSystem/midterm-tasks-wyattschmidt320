925f927b03e40eaf32380ab1cf7a65b5bceb9050
wyattschmidt320 <wyatt.schmidt320@students.cmcss.net>
2025-12-19 09:51:30 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
 
+slices = party_pizza_mini + large + medium
+print(f"Total number of slices: {slices}")
+
+people += 1
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+people += 2 #Eric and Brandon are coming too.
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f" Leftover slices: {leftover}")
+
+
+slices += party_pizza_mini
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+print("...for Mr. Hollow Leg")
